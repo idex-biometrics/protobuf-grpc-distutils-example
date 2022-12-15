4ab78bdbcf7d5f739662481edd765b3f2ebed519
Shareef Jalloq <shareef.jalloq@idexbiometrics.com>
2022-12-15 14:14:40 +0000

    Adding proto file and setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import setuptools.command.build
+
+setuptools.command.build.build.sub_commands.insert(0, ('generate_grpc_py_protobufs', None))
+setuptools.setup(
+    options={
+        'generate_grpc_py_protobufs': {
+            'source_dir':        './proto',
+            'proto_root_path':   './proto',
+            'extra_proto_paths': [],
+            'output_dir':        '.',
+            # 'output_dir':        './src/genesee_protobuf',
+            'proto_files':       [
+                './proto/hello.proto',
+            ],
+        }
+    }
+)
